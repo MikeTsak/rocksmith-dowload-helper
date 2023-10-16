36790257ca6794efaee7c80c8c6d5556c4493559
Mike <32255755+MikeTsak@users.noreply.github.com>
2023-10-16 03:48:37 +0300

    Added GUI

--- rock-helper.py
+++ rock-helper.py
@@ -1,36 +1,60 @@
 import os
 import time
 import shutil
+import PySimpleGUI as sg
+import threading
+import webbrowser
 
-#https://ignition4.customsforge.com/
-
-def move_psarc_files(source_path, destination_path):
+def move_psarc_files(source_path, destination_path, window):
     # Check if both source and destination directories exist
-    if not os.path.exists(source_path):
-        print(f"Source path {source_path} does not exist.")
+    if not os.path.exists(source_path) or not os.path.exists(destination_path):
+        sg.popup_error("Either source or destination path does not exist.")
         return
-
-    if not os.path.exists(destination_path):
-        print(f"Destination path {destination_path} does not exist.")
-        return
-
-    # List files in source directory
-    for file in os.listdir(source_path):
-        if file.endswith('.psarc'):
-            source_file = os.path.join(source_path, file)
-            destination_file = os.path.join(destination_path, file)
-            
-            # Move the file from source to destination
-            shutil.move(source_file, destination_file)
-            print(f"Moved {file} to {destination_path}")
-
-def main():
-    source_path = input("Enter the source path: ")
-    destination_path = input("Enter the destination path: ")
 
     while True:
-        move_psarc_files(source_path, destination_path)
+        # List files in source directory
+        for file in os.listdir(source_path):
+            if file.endswith('.psarc'):
+                source_file = os.path.join(source_path, file)
+                destination_file = os.path.join(destination_path, file)
+                
+                # Move the file from source to destination
+                shutil.move(source_file, destination_file)
+                window.write_event_value('-FILE_MOVED-', file)
+        
         time.sleep(10)  # Sleep for 10 seconds before checking again
+
+def main():
+    layout = [
+        [sg.Text("Enter the source path:"), sg.InputText(key='-SOURCE-'), sg.FolderBrowse()],
+        [sg.Text("Enter the destination path:"), sg.InputText(key='-DEST-'), sg.FolderBrowse()],
+        [sg.Button('Start'), sg.Button('Exit'), sg.Button('Open Site')],
+        [sg.Listbox(values=[], size=(50, 10), key='-FILE_LIST-')],
+        [sg.Text("", size=(50,1), key='-STATUS-')],
+        [sg.Text("Not Started", size=(20,1), key='-INDICATOR-', text_color='red')]
+    ]
+
+    window = sg.Window("PSARC File Mover", layout)
+
+    while True:
+        event, values = window.read()
+        
+        if event == sg.WINDOW_CLOSED or event == 'Exit':
+            break
+        elif event == 'Start':
+            source_path = values['-SOURCE-']
+            dest_path = values['-DEST-']
+            
+            threading.Thread(target=move_psarc_files, args=(source_path, dest_path, window), daemon=True).start()
+            window['-INDICATOR-'].update("Started", text_color='green')
+        elif event == '-FILE_MOVED-':
+            moved_file = values['-FILE_MOVED-']
+            window['-FILE_LIST-'].update([moved_file] + window['-FILE_LIST-'].get_list_values())
+            window['-STATUS-'].update(f"Moved {moved_file} to {dest_path}")
+        elif event == 'Open Site':
+            webbrowser.open("https://ignition4.customsforge.com/")
+
+    window.close()
 
 if __name__ == "__main__":
     main()
